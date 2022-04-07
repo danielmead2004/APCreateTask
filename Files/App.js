69dc14d77c108c08e36b66630fe3dacb8c8690ec
danielmead2004 <92113936+danielmead2004@users.noreply.github.com>
2022-04-07 11:41:33 -0600

    Doneeeeee

---
+++ Files/App.js
@@ -0,0 +1,30 @@
+var dim= 100;
+function makecardholder(w, h, xPos, yPos, id){      //Creates Card Holding Divs
+	b = document.createElement("button");
+	b.style.border = "red solid thin";
+	b.style.width = w +"px";
+	b.style.height = h +"px";
+	b.style.backgroundImage = "url('images/backofcard.jpg')";
+	b.style.position = "absolute";
+	b.style.left = xPos + "px";
+	b.style.top = yPos + "px";
+	b.id = id;
+	//future even listener
+	document.body.append(b); 
+}
+
+var b0 = makecardholder(100, 146, dim*0, dim*1, 0); //calling function to create 2x4 grid 
+var b1 = makecardholder(100, 146, dim*2, dim*1, 0);
+var b2 = makecardholder(100, 146, dim*4, dim*1, 0);
+var b3 = makecardholder(100, 146, dim*6, dim*1, 0);
+var b4 = makecardholder(100, 146, dim*8, dim*1, 0);
+var b5 = makecardholder(100, 146, dim*10, dim*1, 0);
+var b6 = makecardholder(100, 146, dim*12, dim*1, 0);
+var b7 = makecardholder(100, 146, dim*0, dim*3, 0);
+var b8 = makecardholder(100, 146, dim*2, dim*3, 0);
+var b9 = makecardholder(100, 146, dim*4, dim*3, 0);
+var b10 = makecardholder(100, 146, dim*6, dim*3, 0);
+var b11 = makecardholder(100, 146, dim*8, dim*3, 0);
+var b12 = makecardholder(100, 146, dim*10, dim*3, 0);
+var b13 = makecardholder(100, 146, dim*12, dim*3, 0);
+
